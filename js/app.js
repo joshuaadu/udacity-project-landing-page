7d2d4c57996ee1ecc8958695125be34ff3360bf0
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-08-10 15:50:02 +0000

    feat: Check section in viewport & add active state

--- js/app.js
+++ js/app.js
@@ -39,10 +39,27 @@ const sectionData = [
 ];
 
 /**
+ * @description check if section is in viewport with is it bounding client dimensions
+ * @param {*} section 
+ * @returns boolean of whether section is in viewport
+ */
+const isInView = (section) => {
+    const rect = section.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+/**
  * @description Create navbar links with the section titles and Updates the navbar
  */
 const updateNavBar = () => {
+    // Select ul for links
     const navBar = document.querySelector('.nav__links');
+    // iterate through section data and create links in the nav bar
     for (const section of sectionData) {
         let link = document.createElement('li');
         let anchor = document.createElement('a');
@@ -87,19 +104,22 @@ const createSections = () => {
 
 /**
  * @description mark nav link and corresponding section as active
- * @param {*} link 
+ * @param {*} section 
  */
-const activeState = (link) => {
-    link.classList.add('active');
-    document.querySelector(`${link.hash} h2`).classList.add('active');
-};
-
-const removeActiveState = (links) => {
-    links.forEach(link => {
-        link.classList.remove('active');
-        document.querySelector(`${link.hash} h2`).classList.remove('active');
+const activeState = (section) => {
+    // Select heading of the section
+    const heading = document.querySelector(`#${section.id} h2`) || document.querySelector(`#${section.id} h1`);
+    // Select all nav links in the header
+    const links = document.querySelectorAll('.nav__link a');
+    // Iterate through links and mark link with corresponding section as active
+    links.forEach((link) => {
+        console.log(link.hash);
+        if(link.hash == `#${section.id}`){
+            link.classList.add('active');
+            heading.classList.add('active');
+        } else link.classList.remove('active');
     })
-}
+};
 
 /**
  * @description scroll to corresponding section when nav link clicked
@@ -109,10 +129,25 @@ const scrollToSection = () => {
     links.forEach(link => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
-            const href = document.querySelector(link.hash);
-            href.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
-            // removeActiveState(links);
-            activeState(link, href);
+            const section = document.querySelector(link.hash);
+            section.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+            activeState(section);
+        });
+    });
+};
+
+/**
+ * @description Check which section is in Viewport while page is being scrolled and make active
+ */
+const pageScrolling = () => {
+    const sections = document.querySelectorAll('.section__item');
+    document.addEventListener('scroll', () => {
+        console.log('scrolling');
+        sections.forEach((section) => {
+            const heading = document.querySelector(`#${section.id} h2`) || document.querySelector(`#${section.id} h1`);
+            (isInView(section))?
+            activeState(section): 
+            heading.classList.remove('active');
         });
     });
 };
@@ -121,3 +156,4 @@ const scrollToSection = () => {
 updateNavBar();
 createSections();
 scrollToSection();
+pageScrolling();
